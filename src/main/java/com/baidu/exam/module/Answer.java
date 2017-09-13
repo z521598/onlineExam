f029a36a0cebe39d37c417e34517c209da7c092e
langshiquan <langshiquan@baidu.com>
2017-09-13 23:30:25 +0800

    msgservice

--- src/main/java/com/baidu/exam/module/Answer.java
+++ src/main/java/com/baidu/exam/module/Answer.java
@@ -4,4 +4,5 @@ package com.baidu.exam.module;
  * Created by langshiquan on 17/9/13.
  */
 public class Answer {
+
 }
---
+++ src/main/java/com/baidu/exam/service/ExamService.java
@@ -0,0 +1,10 @@
+package com.baidu.exam.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by langshiquan on 17/9/13.
+ */
+@Service
+public class ExamService {
+}
