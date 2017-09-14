c4c806d67ba6cd5d4d258a14dc6574d385c32acb
langshiquan <langshiquan@baidu.com>
2017-09-14 15:06:49 +0800

    msgrequestmap

--- src/main/java/com/baidu/exam/web/PaperController.java
+++ src/main/java/com/baidu/exam/web/PaperController.java
@@ -1,10 +1,12 @@
 package com.baidu.exam.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by langshiquan on 17/9/13.
  */
 @Controller
+@RequestMapping("/paper")
 public class PaperController {
 }
--- src/main/java/com/baidu/exam/web/UserController.java
+++ src/main/java/com/baidu/exam/web/UserController.java
@@ -1,11 +1,13 @@
 package com.baidu.exam.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by langshiquan on 17/9/13.
  */
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 }
