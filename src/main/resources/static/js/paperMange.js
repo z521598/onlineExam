71948c8afd04a4f072939cd7b1cd82bb2c3e6eda
lsq <576506402@qq.com>
2017-09-17 21:40:16 +0800

    msg:paper

--- src/main/resources/static/js/paperMange.js
+++ src/main/resources/static/js/paperMange.js
@@ -73,7 +73,7 @@ function listPaper(data) {
     td1.innerHTML = data.paperId;
     td2.innerHTML = data.title;
     td3.innerHTML = data.author;
-    td4.innerHTML = '<input type = "button" value="删除" onclick="deletePaper(' + data.paperId + ')"/><a href="paperDetail.html?id=' + data.paperId + ' target = "right" class="paperDetail">查看详情</a> ';
+    td4.innerHTML = '<input type = "button" value="删除" onclick="deletePaper(' + data.paperId + ')"/><a href="paperDetail.html?id=' + data.paperId + '" target = "right" class="paperDetail">查看详情</a> ';
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
@@ -92,13 +92,45 @@ function deletePaper(id) {
 ////查看详情
 function paperDetail() {
     var id = UrlParm.parm("id");
-    alert(id);
     $.ajax({
         type: "GET",
         url: "/paper/" + id,
         success: function (data) {
-            console.log(data[id]);
-            $(".head").innerHTML = data[id].title;
+            var paper = eval("("+data+")");
+//            console.log(paper[0].title)
+            var head = document.getElementById("head");
+            var author = document.getElementById("author");
+            var papers = document.getElementById("paper");
+//            console.log(paper[0].questions);
+            var questions = paper[0].questions;
+            head.innerText = paper[0].title;
+            author.innerText += paper[0].author;
+            for(i=0;i<questions.length-1;i++){
+                var div1 = document.createElement("div");
+                var div2 = document.createElement("div");
+                var div3 = document.createElement("div");
+                var div4 = document.createElement("div");
+                div1.className = "title";
+                div2.className = "answer";
+                div3.className = "mark";
+                div4.className = "exercise";
+                div1.innerText = "题目：";
+                div2.innerText = "答案：";
+                div3.innerText = "分数：";
+                div4.appendChild(div1);
+                div4.appendChild(div2);
+                div4.appendChild(div3);
+                papers.appendChild(div4);
+            }
+            var titles = document.querySelectorAll(".title");
+            var answer = document.querySelectorAll(".answer");
+            var mark = document.querySelectorAll(".mark");
+            for(j=0;j<questions.length;j++){
+            console.log(questions[j]);
+                titles[j].innerText += questions[j].question;
+                answer[j].innerText += questions[j].answer;
+                mark[j].innerText += questions[j].mark;
+            }
         }
     });
 }
