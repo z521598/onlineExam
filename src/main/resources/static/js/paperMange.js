bbdfc9cf63f256a4b5c676a4dd839e37e9af74ca
langshiquan <langshiquan@baidu.com>
2017-09-17 20:36:09 +0800

    msg:bugfix

--- src/main/resources/static/js/paperMange.js
+++ src/main/resources/static/js/paperMange.js
@@ -1,44 +1,45 @@
-var i =0;
-$(".add").click(function(){
-    var newPapers = '<div id="div'+i+'"><label>题目：</label><input type="text" name="paperTit"/><label>答案：</label><input type="text" name="answer"/> <label>分数：</label><input type="text" name="mark"/>   <input type="button" value="删除" class="delete" onclick="deletePaper('+i+')"> <br></div>';
+var i = 0;
+$(".add").click(function () {
+    var newPapers = '<div id="div' + i + '"><label>题目：</label><input type="text" name="paperTit"/><label>答案：</label><input type="text" name="answer"/> <label>分数：</label><input type="text" name="mark"/>   <input type="button" value="删除" class="delete" onclick="deletePaper(' + i + ')"> <br></div>';
     $(".papers").append(newPapers);
     i++;
 });
-function deletePaper(id){
-    $("#div"+id).remove();
+function deletePaper(id) {
+    $("#div" + id).remove();
 }
 //保存试卷
-$("#savePapers").click(function(){
+$("#savePapers").click(function () {
     var paper = {};
     var title = $('input[name="title"]').val();
     var author = $('input[name="author"]').val();
     var questions = [];
-    for(i=0;i<$('input[name="paperTit"]').length;i++){
+    for (i = 0; i < $('input[name="paperTit"]').length; i++) {
         var question = {};
         var a = $('input[name="paperTit"]')[i];
-        question.question =  $('input[name="paperTit"]')[i].value;
-        question.answer =  $('input[name="answer"]')[i].value;
-        question.mark =  $('input[name="mark"]')[i].value;
+        question.question = $('input[name="paperTit"]')[i].value;
+        question.answer = $('input[name="answer"]')[i].value;
+        question.mark = $('input[name="mark"]')[i].value;
         questions.push(question);
-    };
+    }
+    ;
     paper.title = title;
     paper.author = author;
     paper.questions = questions;
 //    console.log(paper);
-   $.ajax({
-   		type:"post",
-   		url:"/paper",
-   		dataType: "json",
+    $.ajax({
+        type: "post",
+        url: "/paper",
+        dataType: "json",
         data: JSON.stringify(paper),
         headers: {'Content-Type': 'application/json'},
-   		success:function(res){
-   		    if(res.success){
-   		        alert("保存成功");
-   		    }else{
-   		        alert("保存失败，报错信息："+ res.message);
-   		    }
-   		}
-   	});
+        success: function (res) {
+            if (res.success) {
+                alert("保存成功");
+            } else {
+                alert("保存失败，报错信息：" + res.message);
+            }
+        }
+    });
 });
 //点击试卷管理
 function getPaperInfo(method, url, data) {
@@ -63,43 +64,41 @@ function getPaperInfo(method, url, data) {
     xhr.send(data);
     // xhr.responseText;//响应文本
 }
-function listPaper(data){
-        var tr = document.createElement("tr");
-        var td1 = document.createElement("td");
-        var td2 = document.createElement("td");
-        var td3 = document.createElement("td");
-        var td4 = document.createElement("td");
-        td1.innerHTML = data.paperId;
-        td2.innerHTML = data.title;
-        td3.innerHTML = data.author;
-        td4.innerHTML = '<input type = "button" value="删除" onclick="deletePaper('+data.paperId+')"/><a href="paperDetail.html?id='+data.paperId+' target = "right" class="paperDetail">查看详情</a> ';
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        tr.appendChild(td3);
-        tr.appendChild(td4);
-        $(".tableList").append(tr);
+function listPaper(data) {
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    td1.innerHTML = data.paperId;
+    td2.innerHTML = data.title;
+    td3.innerHTML = data.author;
+    td4.innerHTML = '<input type = "button" value="删除" onclick="deletePaper(' + data.paperId + ')"/><a href="paperDetail.html?id=' + data.paperId + ' target = "right" class="paperDetail">查看详情</a> ';
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    $(".tableList").append(tr);
 };
 function deletePaper(id) {
     $.ajax({
-    		type:"DELETE",
-    		url:"/paper/"+id,
-    		success:function(data){
-    			window.location.reload();
-    		}
+        type: "DELETE",
+        url: "/paper/" + id,
+        success: function (data) {
+            window.location.reload();
+        }
     });
 }
 ////查看详情
-function paperDetail(）{
-
-    var id=UrlParm.parm("id");
-        alert(id);
-
+function paperDetail() {
+    var id = UrlParm.parm("id");
+    alert(id);
     $.ajax({
-        type:"GET",
-        url:"/paper/"+id,
-        success:function(data){
+        type: "GET",
+        url: "/paper/" + id,
+        success: function (data) {
             console.log(data[id]);
-            $(".head").innerHTML =data[id].title;
+            $(".head").innerHTML = data[id].title;
         }
     });
 }
