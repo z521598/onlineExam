2d282184006a9c76580b08c73185938b12dd6784
lsq <576506402@qq.com>
2017-09-18 11:15:04 +0800

    msg:cc

--- src/main/resources/static/js/examUser.js
+++ src/main/resources/static/js/examUser.js
@@ -8,7 +8,7 @@ function init() {
         url: "/exam/getExamOrPaperForUser",
         success: function (data) {
             var papers = eval("(" + data + ")");
-            console.log(papers);
+//            console.log(papers);
             for (var i = 0; i < papers.length; i++) {
                 var tr = document.createElement("tr");
                 var td1 = document.createElement("td");
@@ -18,17 +18,18 @@ function init() {
                 var td5 = document.createElement("td");
                 var title = papers[i].title;
                 var totalMark = papers[i].totalMark;
+                var realMark = papers[i].realMark;
                 var examStatus = papers[i].examStatus;
-                  console.log(papers[i].paperId);
-                                    console.log(papers[i]);
+//                  console.log(papers[i].paperId);
+//                                    console.log(papers[i]);
                 if(examStatus === "NO"){
                     var realMark = 0;
                     var status = "未完成";
                     td5.innerHTML = '<a href="oltest.html?id=' + papers[i].id + '" target = "right" class="oltest">去完成</a>';
                 }else{
-                    var realMark = "xxx";
+                    var realMark = realMark;
                     var status = "已完成";
-                    td5.innerHTML = '<a href="paperDetail.html?id=' + papers.paperId + '" target = "right" class="paperDetail">查看详情</a> ';
+                    td5.innerHTML = '<a href="admin/paperDetail.html?id=' + papers[i].id + '" target = "right" class="paperDetail">查看详情</a> ';
                 }
                 td1.innerText = title;
                 td2.innerText = totalMark;
--- src/main/resources/static/js/oltest.js
+++ src/main/resources/static/js/oltest.js
@@ -15,16 +15,21 @@ function unComplete() {
             var postExam = document.getElementById("postExam");
             var author = document.getElementById("author");
             var papers = document.getElementById("paper");
+            var paperId = document.getElementById("paperId");
+//            var questionId = document.getElementById("questionId");
 //            console.log(paper[0].questions);
             var questions = paper[0].questions;
             head.innerText = paper[0].title;
             author.innerText += paper[0].author;
+            paperId.value = paper[0].paperId;
             for (i = 0; i < questions.length - 1; i++) {
                 var div1 = document.createElement("div");
                 var div2 = document.createElement("div");
                 var label = document.createElement("label");
                 var div3 = document.createElement("input");
                 var div4 = document.createElement("div");
+                var div6 = document.getElementById("questionId");
+                div6.value = questions[i].id;
                 div1.className = "title";
                 div2.className = "mark";
                 div3.className = "answer";
@@ -33,16 +38,18 @@ function unComplete() {
                 div2.innerText = "分数：";
                 label.innerText = "答案：";
                 div3.type = "text";
+                div6.type = "hidden";
                 div4.appendChild(div1);
                 div4.appendChild(div2);
                 div4.appendChild(label);
                 div4.appendChild(div3);
+                div4.appendChild(div6);
                 papers.appendChild(div4);
             }
             var div5 = document.createElement("input");
             div5.value = "交卷";
             div5.type = "submit";
-            papers.appendChild(div5);
+//            papers.appendChild(div5);
             var titles = document.querySelectorAll(".title");
             var answer = document.querySelectorAll(".answer");
             var mark = document.querySelectorAll(".mark");
