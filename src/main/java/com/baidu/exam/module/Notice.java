1922f370bfadb3bcc30bcb71b116b122b742e84f
langshiquan <langshiquan@baidu.com>
2017-12-21 19:01:33 +0800

    msg:

--- src/main/java/com/baidu/exam/module/Notice.java
+++ src/main/java/com/baidu/exam/module/Notice.java
@@ -8,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.xml.crypto.Data;
 
 /**
  * Created by langshiquan on 17/9/13.
